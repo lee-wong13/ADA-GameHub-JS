e19c3a6d165e47625ac12f5c4f1dfbfc18d74a02
Ada Linn Taule <lee-wong13@hotmail.com>
2026-02-17 21:24:44 +0100

    fix style for game detail page

--- js/product.js
+++ js/product.js
@@ -1,31 +1,29 @@
 let AllGames = [];
 const fallbackImage = "https://via.placeholder.com/300x170?text=No+Image";
-const detailContainer = document.getElementById("game-detail")
-
+const detailContainer = document.getElementById("game-detail");
 
 //Functions
 async function fetchGames() {
   const url = "https://v2.api.noroff.dev/gamehub";
 
-  const params = new URLSearchParams(window.location.search)
-  const id = params.get("id")
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
 
   try {
     const response = await fetch(`${url}/${id}`);
     const result = await response.json();
     const game = result.data;
 
-    console.log(game)
+    console.log(game);
 
-    renderProductDetails(game)
-    
+    renderProductDetails(game);
   } catch (error) {
     console.error("Error fetching games:", error);
   }
 }
 
 function renderProductDetails(game) {
-  console.log('render game')
+  console.log("render game");
   detailContainer.innerHTML = `
     <div class="game-container">
       <nav class="breadcrumb">Games &gt; ${game.genre} &gt; ${game.title}</nav>
